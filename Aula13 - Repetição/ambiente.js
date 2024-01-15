f41532e7c9e0c7d79c9cfed699c878b6bb937a53
Beacesconetto <beacesconetto203@gmail.com>
2024-01-15 16:31:23 -0300

    Repetição-do-while

---
+++ Aula13 - Repetição/ambiente.js
@@ -0,0 +1,14 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while ( c <= 6)
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+Mais encontrada nos códigos
+*/
